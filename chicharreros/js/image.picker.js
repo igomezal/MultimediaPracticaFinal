445f398a110baa0e93050a66bb19d3bf8339e73e
Alvaro95pa <a.pavonal@alumnos.urjc.es>
2017-04-23 17:30:11 +0200

    Version final de interes.html

--- chicharreros/js/image.picker.js
+++ chicharreros/js/image.picker.js
@@ -2,12 +2,11 @@ $(document).ready(function() {
     var selected = false;
     
     $('ul > li > img').click(function() {
-        $('ul > li > img').removeClass('image-selected');
-        $(this).addClass('image-selected');
         selected = true;
     });
     
-    $('#submit').click(function() {
+    $('#submit').click(function(event) {
+        event.preventDefault();
         if(selected && $('#email').val()) {
             $('#form').append('<div id="post" class="alert alert-success alert-dismissible" role="alert"><button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button><span>La postal ha sido enviada con éxito</span></div>');
             window.setTimeout(function() { $("#post").alert('close'); }, 2000);
